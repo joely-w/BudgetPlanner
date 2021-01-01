b29eaa4a4fab9b4369219d4f0d5a5c3066d4e665
joely-w <joely_w@outlook.com>
2021-01-01 12:37:32 +0000

    Interfaced with registration service layer

--- api/users.js
+++ api/users.js
@@ -1,10 +1,17 @@
 /* Router for login/registration requests */
 
 const express = require('express');
+const Register = require('../services/register.js');
+
 const router = express.Router();
 
-router.get('/login', function (req, res) {
-    res.send('No login yet');
-})
+router.get('/login', (req, res) => {
+  const registration = new Register(req.body.user);
+  if (registration.registerUser()) {
+    res.send({ success: true });
+  } else {
+    res.send({ success: false });
+  }
+});
 
 module.exports = router;
