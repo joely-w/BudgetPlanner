50573ef5d0734c15aaa06cf98791a2abeaed7fec
joely-w <joely_w@outlook.com>
2021-01-01 14:05:57 +0000

    Created registration service class

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const Database = require('./database');
+
+class RegisterDB extends Database {
+  /**
+   * Class constructor
+   * @param {object} data
+   * @param {string} data.first_name
+   * @param {string} data.last_name
+   * @param {string} data.email
+   * @param {string} data.password
+   */
+  constructor(data) {
+    super();
+    this.email = data.email;
+    this.password = data.password;
+    this.name = { first: data.first_name, last: data.last_name };
+  }
+
+  register() {
+    this.connection.query('INSERT INTO users(email, password, first_name, last_name) VALUES(?,?,?,?)',
+      [this.email, this.password, this.name.first, this.name.last]);
+  }
+}
+module.exports = RegisterDB;
