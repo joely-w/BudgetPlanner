1c342fd32c4d15d0bb2dfc6e855b62fed16c2e7a
joely-w <joely_w@outlook.com>
2021-01-01 12:32:57 +0000

    Initial registration service class

---
+++ services/register.js
@@ -0,0 +1,40 @@
+const crypto = require('crypto');
+const RegisterDB = require('../models/user');
+
+class Register extends RegisterDB {
+  constructor(data) {
+    super(data);
+  }
+
+  validateEmail() {
+    const emailFormat = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$";
+    if (!this.email.test(emailFormat)) {
+      this.error = 'Email is invalid';
+      return false;
+    }
+    return true;
+  }
+
+  validateName() {
+    if (this.name.first.length > 128 || this.name.last.length > 128) {
+      this.error = 'First or last name is too long';
+      return false;
+    }
+    return true;
+  }
+
+  hashPassword() {
+    this.password = crypto.createHash('sha256').update(this.password).digest('hex');
+    return true;
+  }
+
+  registerUser() {
+    if (this.validateEmail() && this.validateName() && this.hashPassword()) {
+      this.register();
+      return true;
+    }
+    return false;
+  }
+}
+
+module.exports = Register;
