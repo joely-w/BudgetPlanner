873bd9614bf4cf675afc94f66feefe6f71f22e02
joely-w <joely_w@outlook.com>
2021-01-01 12:31:47 +0000

    Updated database exports

--- config/index.js
+++ config/index.js
@@ -5,5 +5,7 @@ module.exports = {
   database: {
     host: environment.DB_HOST,
     username: environment.DB_USERNAME,
+    password: environment.DB_PASSWORD,
+    name: environment.DB_NAME,
   },
 };
