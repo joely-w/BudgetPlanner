553965b3e27e29fa61af2c53a25c0eb275afd7a1
joely-w <joely_w@outlook.com>
2021-01-02 18:09:59 +0000

    Added form validation calls and AJAX post

--- web/scripts/register.js
+++ web/scripts/register.js
@@ -1,3 +1,26 @@
-function validate() {
+$(document).ready(() => {
+  $('#register').on('submit', () => {
+    const registrationForm = $('#register');
 
-}
+    if (!registrationForm[0].checkValidity()) {
+      $('#register_button').click();
+      return false;
+    }
+    const data = {
+      user: {
+        email: $('#email').val(),
+        password: $('#password').val(),
+        first_name: $('#first_name').val(),
+        last_name: $('#last_name').val(),
+      },
+    };
+    $.post('/api/register', data, (response) => {
+      if (response.success) {
+        $('#form').html('<h1>Registration successful!</h1><span>You can now <a href="login.html">login</a></span>');
+      } else {
+        $('#error').html(`<strong class="red">Error:</strong> ${response.error}`);
+      }
+    });
+    return false;
+  });
+});
