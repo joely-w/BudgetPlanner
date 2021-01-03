7f4bcb9ba6a8e43cb85c608acb3a543d69f59558
joely-w <joely_w@outlook.com>
2021-01-03 00:15:26 +0000

    Updated to correct database model

--- services/register.js
+++ services/register.js
@@ -1,5 +1,5 @@
 const crypto = require('crypto');
-const RegisterDB = require('../models/user');
+const RegisterDB = require('../models/register');
 
 class Register extends RegisterDB {
   constructor(data) {
