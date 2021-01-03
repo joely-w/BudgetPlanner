ee32b638adde341889a00e78b5004040286801a2
Joel Winterton <V20550@gapps.stokesfc.ac.uk>
2021-01-03 02:01:38 +0000

    Added comments

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
-
 const bodyParser = require('body-parser');
 const userRouter = require('./api/users');
 
 const app = express();
 
+// Use body-parser to parse post requests
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Use router for account system
 app.use('/api', userRouter);
+// Set web directory as static
 app.use(express.static('./web'));
 
 const server = app.listen(8081, '127.0.0.1', () => {
