b084987afbf06036dfac6b216ae377457a855419
Joel Winterton <V20550@gapps.stokesfc.ac.uk>
2021-01-03 02:03:32 +0000

    Implemented express session Changed login to async

--- api/users.js
+++ api/users.js
@@ -1,10 +1,17 @@
 /* Router for login/registration requests */
-
 const express = require('express');
+const session = require('express-session');
+const config = require('../config');
 const Register = require('../services/register');
 const User = require('../services/users');
 
 const router = express.Router();
+// Use express session middleware
+router.use(session({
+  secret: config.session.secret,
+  resave: false,
+  saveUninitialized: false,
+}));
 
 router.post('/register', (req, res) => {
   const registration = new Register(req.body.user);
@@ -14,15 +21,10 @@ router.post('/register', (req, res) => {
     res.send({ success: false, error: registration.error });
   }
 });
-router.post('/login', (req, res) => {
+
+router.post('/login', async (req, res) => {
   const user = new User(req.body.email, req.body.password);
-  user.login((result) => {
-    if (result) {
-      res.send({ success: true, result });
-    } else {
-      res.send({ success: false });
-    }
-  });
+  res.send(await user.attemptLogin());
 });
 
 module.exports = router;
