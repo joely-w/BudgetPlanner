f9e77e685c6e69b80d358099930a2e873c138270
Joel Winterton <V20550@gapps.stokesfc.ac.uk>
2021-01-03 02:02:04 +0000

    Changed to promise based mysql

--- models/database.js
+++ models/database.js
@@ -1,15 +1,18 @@
-/** Database connector class * */
+/** Database connector class */
 
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const config = require('../config');
 
 class Database {
   constructor() {
-    this.connection = mysql.createConnection({
+    this.connection = mysql.createPool({
       host: config.database.host,
       user: config.database.username,
       database: config.database.name,
       password: config.database.password,
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0,
     });
   }
 }
