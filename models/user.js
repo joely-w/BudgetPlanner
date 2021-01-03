311c0a906e7bdfbf22e9a60b200109a029d0ba0b
joely-w <joely_w@outlook.com>
2021-01-03 00:16:57 +0000

    Now includes method to query user login

--- models/user.js
+++ models/user.js
@@ -1,24 +1,21 @@
+const crypto = require('crypto');
 const Database = require('./database');
 
-class RegisterDB extends Database {
+class UserDB extends Database {
   /**
-   * Class constructor
-   * @param {object} data
-   * @param {string} data.first_name
-   * @param {string} data.last_name
-   * @param {string} data.email
-   * @param {string} data.password
-   */
-  constructor(data) {
+     * Class constructor
+     * @param {string} email - User email
+     * @param {string} password - User plaintext password
+     */
+  constructor(email, password) {
     super();
-    this.email = data.email;
-    this.password = data.password;
-    this.name = { first: data.first_name, last: data.last_name };
+    this.email = email;
+    this.password = password;
   }
 
-  register() {
-    this.connection.query('INSERT INTO users(email, password, first_name, last_name) VALUES(?,?,?,?)',
-      [this.email, this.password, this.name.first, this.name.last]);
+  login(callback) {
+    this.connection.query('SELECT * FROM users WHERE email = ? and password = ?', [this.email, this.password], (result) => callback(result));
   }
 }
-module.exports = RegisterDB;
+
+module.exports = UserDB;
