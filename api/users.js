3e51c3f168adb2c6f694ecc1be73002b658f347d
joely-w <joely_w@outlook.com>
2021-01-03 00:17:18 +0000

    Added a login route

--- api/users.js
+++ api/users.js
@@ -1,7 +1,8 @@
 /* Router for login/registration requests */
 
 const express = require('express');
-const Register = require('../services/register.js');
+const Register = require('../services/register');
+const User = require('../services/users');
 
 const router = express.Router();
 
@@ -12,6 +13,16 @@ router.post('/register', (req, res) => {
   } else {
     res.send({ success: false, error: registration.error });
   }
+});
+router.post('/login', (req, res) => {
+  const user = new User(req.body.email, req.body.password);
+  user.login((result) => {
+    if (result) {
+      res.send({ success: true, result });
+    } else {
+      res.send({ success: false });
+    }
+  });
 });
 
 module.exports = router;
