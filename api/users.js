069f45602cf2853a87c69d7e666ab5425e210a35
Joel Winterton <V20550@gapps.stokesfc.ac.uk>
2021-01-10 14:04:46 +0000

    All routes switched to async Login method now functional Logout method added Comments added

--- api/users.js
+++ api/users.js
@@ -6,6 +6,7 @@ const Register = require('../services/register');
 const User = require('../services/users');
 
 const router = express.Router();
+
 // Use express session middleware
 router.use(session({
   secret: config.session.secret,
@@ -13,18 +14,47 @@ router.use(session({
   saveUninitialized: false,
 }));
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
+  // Instantiate registration object with data
   const registration = new Register(req.body.user);
-  if (registration.registerUser()) {
+  // Attempt to register user
+  const userRegistration = await registration.registerUser();
+  // Report if successful
+  if (userRegistration) {
     res.send({ success: true });
   } else {
-    res.send({ success: false, error: registration.error });
+    res.send({
+      success: false,
+      error: registration.error,
+    });
   }
 });
 
 router.post('/login', async (req, res) => {
+  // Instantiate user object with login details
   const user = new User(req.body.email, req.body.password);
-  res.send(await user.attemptLogin());
+  // Attempt login
+  const loginDetails = await user.attemptLogin();
+  // If login successful, set session data
+  if (loginDetails.logged_in) {
+    req.session.userData = loginDetails;
+  }
+  res.send(loginDetails);
+});
+
+router.post('/status', (req, res) => {
+  // Check if session data exists
+  if ('userData' in req.session) {
+    res.send(req.session.userData);
+  } else {
+    res.send({ logged_in: false });
+  }
+});
+
+router.post('/logout', (req, res) => {
+  // Unset session data
+  delete req.session.userData;
+  res.send({ success: true });
 });
 
 module.exports = router;
